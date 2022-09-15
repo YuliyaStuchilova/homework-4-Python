a6eb1cdf7ad9d51e55dd1d5c65ec38083e004e07
YuliyaStuchilova <yulia.s9314@gmail.com>
2022-09-15 20:00:12 +0300

    add task4

--- task4.py
+++ task4.py
@@ -1,4 +1,4 @@
-# Задана натуральная степень k. 
+# Задана натуральная степень k.
 # Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена
 #  и записать в файл многочлен степени k.
 
@@ -6,5 +6,33 @@
 
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-k=int(input())
+from random import randint
+k = int(input())
 
+arr = []
+flag = True
+f = open('file.txt', 'a')
+while flag:
+    n = randint(0, 100)
+    if k > 0:
+        if n == 0:
+            f.write('')
+        else:
+            if n == 1:
+                f.write(f'x^{k}')
+                k -= 1
+            else:
+                f.write(f'{n}*x^{k}+')
+                k -= 1
+    elif k == 0:
+        if n != 0:
+            f.write(f'{n}')
+            k -= 1
+        else:
+            f.write('')
+    elif k == -1:
+        f.write('=0')
+        flag = False
+
+
+f.close()
